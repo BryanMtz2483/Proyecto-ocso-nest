6692846b289e58c8ae8277c6804f3e645a6f204d
BryanMtz2483 <mhzb3072@gmail.com>
2025-09-15 19:53:37 -0600

    Implementado CRUD para empleados

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+
+@Injectable()
+export class EmployeesService {
+  private employees: CreateEmployeeDto[] = [{
+    id:1,
+    name:"Alberto",
+    lastName: "Costas",
+    phoneNumber:"XXXX2243"
+  },{
+    id:2,
+    name:"José",
+    lastName: "Pérez",
+    phoneNumber:"33445665"
+  }]
+  create(createEmployeeDto: CreateEmployeeDto) {
+    createEmployeeDto.id = this.employees.length+1;
+    this.employees.push(createEmployeeDto);
+    return "Empleado Creado Exisotamente"
+  }
+
+  findAll() {
+    return this.employees;
+  }
+
+  findOne(id: number) {
+    const employee = this.employees.filter((employee)=>employee.id === id)[0];
+    return employee;
+  }
+
+  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    let employeeToUpdate = this.findOne(id);
+    employeeToUpdate = {
+        ...employeeToUpdate,
+        ...updateEmployeeDto,
+    }
+    this.employees = this.employees.map((employee) => {
+      if (employee.id == id){
+        employee = employeeToUpdate;
+      }
+      return employee;
+    })
+    return employeeToUpdate;
+  }
+
+  remove(id: number) {
+    this.employees = this.employees.filter((employee) => employee.id !== id );
+    return this.employees;
+  }
+}
