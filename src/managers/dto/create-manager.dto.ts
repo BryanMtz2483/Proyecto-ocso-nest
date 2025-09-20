f50d4c8a785c95cc455a3a6d75b3bd48ece1df77
BryanMtz2483 <mhzb3072@gmail.com>
2025-09-20 08:52:49 -0600

    Agregados los recursos de Regiones y managers e implementado CRUD casi completo

--- src/managers/dto/create-manager.dto.ts
+++ src/managers/dto/create-manager.dto.ts
@@ -1,1 +1,17 @@
-export class CreateManagerDto {}
+import { IsEmail, IsNumber, IsString, MaxLength } from "class-validator";
+import { Manager } from "../entities/manager.entity";
+import { OmitType } from "@nestjs/mapped-types";
+
+export class CreateManagerDto extends OmitType(Manager, ['managerId' as const]){
+    @IsString()
+    @MaxLength(80)
+    declare managerFullName: string;
+    @IsString()
+    @IsEmail()
+    declare managerEmail: string;
+    @IsNumber()
+    declare managerSalary: number;
+    @IsString()
+    @MaxLength(16)
+    declare managerPhoneNumber: string;
+}
--- src/regions/dto/create-region.dto.ts
+++ src/regions/dto/create-region.dto.ts
@@ -1,1 +1,12 @@
-export class CreateRegionDto {}
+import { IsArray, IsString, MaxLength } from "class-validator";
+import { Region } from "../entities/region.entity";
+import { OmitType } from "@nestjs/mapped-types";
+
+export class CreateRegionDto extends OmitType(Region, ['regionId'] as const) {
+  @IsString()
+  @MaxLength(100)
+  regionName: string;
+
+  @IsArray()
+  regionStates: string[];
+}
