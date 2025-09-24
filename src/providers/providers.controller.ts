39451ab1cfdc79208ed41c34fed0c5efc46e5e18
BryanMtz2483 <mhzb3072@gmail.com>
2025-09-23 19:12:15 -0600

    Roles Guard implementado

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -5,7 +5,10 @@ import { UpdateProviderDto } from './dto/update-provider.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { UserData } from 'src/auth/decorators/user.decorator';
 import { User } from 'src/auth/entities/user.entity';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
+@UseGuards(AuthGuard)
 @Controller('providers')
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
@@ -14,7 +17,8 @@ export class ProvidersController {
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
-  @UseGuards(AuthGuard)
+  @Roles(["Admin"])
+  @UseGuards(RolesGuard)
   @Get()
   findAll(@UserData() user: User) {
     if (user.userRoles.includes("Employee")) throw new UnauthorizedException("No estás Autorizado, Solo admins y Managers")
